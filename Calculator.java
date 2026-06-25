1bf4b018341882b65e0c80e899a2b6314a400121
nadia1726 <malihatabassumnadia206@gmail.com>
2026-06-25 23:18:23 +0600

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+
+package without.user.input.calculator;
+public class Calculator {
+    public static void main(String[] args){
+        int a=100;
+        int b= 20;
+        System.out.println ("a="+a);
+        System.out.println ("b="+b);
+        System.out.println ("Addition ="+(a+b));
+        System.out.println ("Subtraction="+(a-b));
+        System.out.println ("Multiplication="+(a*b));
+        System.out.println ("Division="+(a/b));
+        
+     
+    }
+    
+}

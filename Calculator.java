1b79204aae6815cd695f8b11261525715749c5f0
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-06-26 13:09:06 +0600

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,21 @@
+package calculator;
 
-package without.user.input.calculator;
+import java.util.Scanner;
+
 public class Calculator {
-    public static void main(String[] args){
-        int a=100;
-        int b= 20;
-        System.out.println ("a="+a);
-        System.out.println ("b="+b);
-        System.out.println ("Addition ="+(a+b));
-        System.out.println ("Subtraction="+(a-b));
-        System.out.println ("Multiplication="+(a*b));
-        System.out.println ("Division="+(a/b));
-        
-     
+
+    public static void main(String[] args) {
+        System.out.print("First number:");
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        System.out.print("Second number:");
+        int b = input.nextInt();
+        System.out.println(" Addition=" + (a + b));
+        System.out.println(" Subtraction=" + (a - b));
+        System.out.println(" Multiplication=" + (a * b));
+        System.out.println(" Division=" + (a / b));
+
     }
-    
+
 }

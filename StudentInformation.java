c54ef4bf97179ca70d4c29a1bfd77114b4aeb9fd
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-06-28 13:27:30 +0600

    StudentInformation.java

--- StudentInformation.java
+++ StudentInformation.java
@@ -1,27 +1,23 @@
-
-package student.information;
+package student.management;
 import java.util.Scanner;
 
-public class StudentInformation {
-
+public class StudentManagement {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
-        System.out.print ("Enter your name:");
-        String name = input. nextLine();
-         System.out.print ("Enter your Student Id:");
-         int id=input.nextInt();
-          System.out.print ("Enter your Department:");
-           String dep = input .nextLine();
-           input.nextLine();
-         System.out.print ("Enter your CGPA:");
-         double cgpa=input.nextDouble();
-         System.out.println ("name:"+name);
-         System.out.println ("id:"+id);
-         System.out.print("department:\n"+dep);
-         System.out.println ("CGPA:"+cgpa);
-         
-         
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Student Name: ");
+        String name = input.nextLine();
+        System.out.print("Enter Student ID: ");
+        int id = input.nextInt();
+        input.nextLine();
+        System.out.print("Enter Department: ");
+        String department = input.nextLine();
+        System.out.print("Enter CGPA: ");
+        double cgpa = input.nextDouble();
+        System.out.println(" Student Information ");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Department: " + department);
+        System.out.println("CGPA: " + cgpa);
+        input.close();
     }
-    
 }

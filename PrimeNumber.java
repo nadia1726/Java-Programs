59f54d77bd00a8587c1a53a233245bc1e4ee08ed
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-06-28 13:17:45 +0600

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,18 +1,2 @@
 
-package primenumber;
-import java.util.Scanner;
-public class PrimeNumber {
-    public static void main(String[] args) {
-        System.out.print("Enter the Number : ");
-        Scanner input=new Scanner(System.in);
-        int number = input.nextInt();
-        if(number % 2 !=0){
-            System.out.print("Prime number"+number);}
-            else {
-                   System.out.println(" Not Prime number"+number); 
-                    }
-            
-        }
-    }
     
-

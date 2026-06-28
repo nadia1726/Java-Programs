54ad24b99404a5f27abad5e5bc06d61f3a0a4c3a
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-06-28 13:43:20 +0600

    Add files via upload

---
+++ UnaryOperetor.java
@@ -0,0 +1,19 @@
+
+package unaryoperetor;
+
+public class UnaryOperetor {
+
+    public static void main(String[] args) {
+      int x=10;
+int result=-x;
+     int y=10;
+    int result1=+y;
+System.out.println( "result x ="+result);
+System.out.println("result y ="+result1);
+System.out.println("result x++ ="+(x++));
+System.out.println("result y-- ="+(y--));
+System.out.println("result ++x ="+(++x));
+System.out.println("result --y ="+(--y));
+    }
+    
+}

84d9cac4bc6e26419617f84b5b0c4de6c57e0c58
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-06-28 13:22:21 +0600

    TemperatureConvert.java

--- TemperatureConvert.java
+++ TemperatureConvert.java
@@ -1,22 +1,18 @@
-
 package temperatureconvert;
-import java. util.Scanner; 
+import java.util.Scanner;
 
 public class TemperatureConvert {
-
-   
     public static void main(String[] args) {
-        System.out.print("Enter Temperature In Celsius :");
-       Scanner input = new Scanner (System.in);
-       double cels= input.nextDouble();
-       double farn=(1.8*cels)+32;
-       System.out.println("Temperature in Fahrenheit="+farn);
-      
-       System.out.print("Enter Temperature In Fahrenheit: ");
-        double fahrenheit= input.nextDouble();
-       double celsius=0.555555*(fahrenheit-32);
-        System.out.println("Temperature in Celsius="+celsius);
-       
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Temperature In Celsius: ");
+        double cels = input.nextDouble();
+        double farn = (1.8 * cels) + 32;
+        System.out.println("Temperature in Fahrenheit = " + farn);
+        System.out.print("Enter Temperature In Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+        double celsius = (fahrenheit - 32) * 5 / 9; 
+        System.out.println("Temperature in Celsius = " + celsius);
+        input.close(); 
     }
-    
 }
+

e2665d3695cbba1c5ca916d419e19b4fcd7cd2b4
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-06-29 22:58:47 +0600

    Add files via upload

---
+++ Switchcase.java
@@ -0,0 +1,50 @@
+
+package switchcase;
+import java.util.Scanner;
+
+public class Switchcase {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter day number (1-7): ");
+        int day = input.nextInt();
+
+        switch (day) {
+
+            case 1:
+                System.out.println("Saturday");
+                break;
+
+            case 2:
+                System.out.println("Sunday");
+                break;
+
+            case 3:
+                System.out.println("Monday");
+                break;
+
+            case 4:
+                System.out.println("Tuesday");
+                break;
+
+            case 5:
+                System.out.println("Wednesday");
+                break;
+
+            case 6:
+                System.out.println("Thursday");
+                break;
+
+            case 7:
+                System.out.println("Friday");
+                break;
+
+            default:
+                System.out.println("Invalid day number");
+        }
+
+        input.close();
+    }
+}

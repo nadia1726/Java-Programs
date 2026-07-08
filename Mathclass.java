35072aa1742dc2c32fe3b0ee442766d3bc87ff8e
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-07-08 22:48:36 +0600

    Add files via upload

---
+++ Mathclass.java
@@ -0,0 +1,28 @@
+
+package mathclass;
+public class Mathclass {
+    public static void main(String[] args) {
+        int x = 5;
+        int y=8;
+        int z=-100;
+      double a =4.4;
+      int b=64;
+      double pi=Math.PI;
+         System.out.print("maximum value = ");
+        System.out.println(Math.max(x,y));
+         System.out.print("minimum value = ");
+        System.out.println(Math.min(x,y));
+         System.out.print("absoulte value = ");
+        System.out.println(Math.abs(z));
+         System.out.print("x to the power y = ");
+        System.out.println(Math.pow(x,y));
+         System.out.print("round value a = ");
+        System.out.println(Math.round (a));
+         System.out.print("square root of b = ");
+        System.out.println(Math.sqrt(b));
+        System.out.println("PI = " +pi);
+        
+        
+    }
+    
+}

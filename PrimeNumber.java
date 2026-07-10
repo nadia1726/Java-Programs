03275d9ecaaa5f4865d2b2a7ade5ff9b329c8fe4
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-07-10 13:12:02 +0600

    PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,20 +1,27 @@
 package primenumber;
+
 import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter the Number: ");
         int number = input.nextInt();
 
         int count = 0;
 
-        for (int i = 1; i <= number; i++) {
+        for (int i = 2; i < number; i++) {  ে
             if (number % i == 0) {
                 count++;
             }
         }
-        System.out.println(number + (count == 2 ? " is Prime number" : " is Not Prime number"));
+
+        if (count == 0 && number > 1) {
+            System.out.println(number + " is Prime number");
+        } else {
+            System.out.println(number + " is Not Prime number");
+        }
 
         input.close();
     }

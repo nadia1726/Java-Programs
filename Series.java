827b1eb5ba537bdffefc37f491cfc195fad69f3b
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-07-10 11:13:05 +0600

    Add files via upload

---
+++ Series.java
@@ -0,0 +1,22 @@
+
+package series;
+import java.util.Scanner;
+public class Series {
+    public static void main(String[] args) {
+        System.out.print("enter the first number :");
+        Scanner input = new Scanner(System.in);
+          int m= input.nextInt();
+        int sum=0;
+        System.out.print("enter the last number :");
+        int n= input.nextInt();
+        for (int i = m; i <= n; i=i+3) {
+            System.out.print(i+ "  ");
+            sum=sum+i;
+           
+        }
+        System.out.println("");
+        System.out.println("sum = " + sum);
+        
+    }
+    
+}

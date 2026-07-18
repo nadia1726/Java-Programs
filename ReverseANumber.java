d3674cec7c9de3a44d31d6407c835b994e27bc8a
Maliha Tabassum Nadia <malihatabassumnadia206@gmail.com>
2026-07-18 16:34:30 +0600

    Add files via upload

--- ReverseANumber.java
+++ ReverseANumber.java
@@ -1,19 +1,26 @@
-
 package reverse.a.number;
+
 import java.util.Scanner;
+
 public class ReverseANumber {
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); 
-        int num,sum=0,temp,rem;
-        num=input.nextInt();
-        temp=num;
-        while(temp!=0){
-            rem = temp %10;
-            sum=sum*10+rem;
-            temp = temp/10;
-            
+        Scanner input = new Scanner(System.in);
+        int num, sum = 0, temp, rem;
+        num = input.nextInt();
+        temp = num;
+        while (temp != 0) {
+            rem = temp % 10;
+            sum = sum * 10 + rem;
+            temp = temp / 10;
+
         }
-        System.out.println("Reverse A Number = "+sum);
+        if (num==sum){
+            System.out.println(num+ " palindrom");
+        }
+        else{
+        System.out.println(num+"  not palindrom " );
     }
-    
+
+}
 }
